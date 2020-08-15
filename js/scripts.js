69332ae1a5e7a542cfa1893dc92c53bfc668ae15
GeronimoOlanda <51466744+GeronimoOlanda@users.noreply.github.com>
2020-08-15 20:05:11 -0300

    atualizando as direções

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ let box = 32;
 
 let snake = [];
 snake[0] = {
-    x:8*box,
-    y:8*box
+    x:8 * box,
+    y:8 * box
 }
 
 let direction = "right";
@@ -22,9 +22,41 @@ let criarCobrinha = () => {
     }
 }
 
+
+let update = (event) =>{
+
+    if(event.keyCode == 37 && direction != "right"){
+        direction = "left";
+    }
+    if(event.keyCode == 38 && direction != "down"){
+        direction = "up";
+    }
+    if(event.keyCode == 39 && direction != "left"){
+        direction = "right";
+    }
+    if(event.keyCode == 40 && direction != "up"){
+        direction = "down";
+    }
+}
+document.addEventListener('keydown', update);
+
 let iniciarJogo = () =>{
+    if(snake[0].x > 15 * box && direction == "right"){
+        snake[0].x = 0;
+    }
+    if(snake[0].x < 0 * box && direction == "left"){
+        snake[0].x = 16 * box;
+    }
+    if(snake[0].y > 15 * box && direction == "down"){
+        snake[0].y = 0;
+    }
+    if(snake[0].y < 0 * box && direction == "up"){
+        snake[0].y = 16 * box;
+    }
+
     criarBG();
     criarCobrinha();
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
